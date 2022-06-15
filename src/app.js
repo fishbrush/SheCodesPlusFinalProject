3fa71cac218e748a72be30ca2a1ab109a5151ae2
fishbrush <93577828+fishbrush@users.noreply.github.com>
2022-06-15 14:01:11 +1200

    Display forecast data using api

--- src/app.js
+++ src/app.js
@@ -21,26 +21,47 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp){
+let date = new Date (timestamp*1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+return days[day];
+}
+
+function displayForecast(response) {
+let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHtml = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="col-2">
+  forecast.forEach(function (forecastDay,index) {
+    if (index < 5){
+      forecastHtml =
+        forecastHtml +
+        `<div class="col-2">
                 <div class="weather-forecast-date">
-                ${day}
+                ${formatDay(forecastDay.dt)}
                 </div>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="50px">
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" width="50px">
                 <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">18° </span> <span class="weather-forecast-temperature-min">12°</span> 
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}° </span> <span class="weather-forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span> 
               </div>
             </div>`;
+    }
   });
   forecastHtml = forecastHtml + `</div>`;
   forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(coordinates){
+    let apiKey = "90bc7e62af1e08f18b00cf6e3cfcd85b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -63,6 +84,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -78,7 +101,6 @@ function handelSubmit(event) {
 }
 
 search("Suva");
-displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handelSubmit);
